62b348a99e8e4651bf6cc10a3ceb5d48decbb62d
Anatolii Yarovyi <apyarovyi@gmail.com>
2021-08-25 23:56:34 +0300

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,72 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
-const contactsOperations = require('../../model')
-const { contactSchema } = require('../../validation')
+const ctrl = require('../../controllers/contacts')
 
-router.get('/', async (req, res, next) => {
-  try {
-    const contacts = await contactsOperations.listContacts()
-    res.json({ contacts })
-  } catch (error) {
-    next(error)
-  }
-})
+router.get('/', ctrl.listContacts)
 
-router.get('/:contactId', async (req, res, next) => {
-  try {
-    const { contactId } = req.params
-    const contact = await contactsOperations.getContactById(contactId)
-    if (!contact) {
-      return res.status(404).json({ message: 'Not found' })
-    }
-    res.json({ contact })
-  } catch (error) {
-    next(error)
-  }
-})
+router.get('/:contactId', ctrl.getContactById)
 
-router.post('/', async (req, res, next) => {
-  try {
-    const { error } = await contactSchema.validate(req.body)
-    if (error) {
-      return res.status(400).json({ message: 'missing required name field' })
-    }
-    const contacts = await contactsOperations.addContact(req.body)
-    res.status(201).json({ contacts })
-  } catch (error) {
-    next(error)
-  }
-})
+router.post('/', ctrl.addContact)
 
-router.delete('/:contactId', async (req, res, next) => {
-  try {
-    const { contactId } = req.params
-    const remove = await contactsOperations.removeContact(contactId)
-    if (!remove) {
-      return res.status(404).json({ message: 'Not found' })
-    }
-    res.json({ remove })
-  } catch (error) {
-    next(error)
-  }
-})
+router.delete('/:contactId', ctrl.removeContact)
 
-router.put('/:contactId', async (req, res, next) => {
-  try {
-    const { error } = await contactSchema.validate(req.body)
-    if (error) {
-      return res.status(400).json({ message: 'missing required name field' })
-    }
-    const { contactId } = req.params
-    const contact = await contactsOperations.updateContact(contactId, req.body)
-    if (!contact) {
-      return res.status(404).json({ message: 'Not found' })
-    }
-    res.status(201).json({ contact })
-  } catch (error) {
-    next(error)
-  }
-})
+router.put('/:contactId', ctrl.updateContact)
 
 module.exports = router
