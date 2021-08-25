f769a8b42853abd700e93ba0150f2f805a14902f
Anatolii Yarovyi <apyarovyi@gmail.com>
2021-08-25 23:27:28 +0300

    Update removeContact.js

--- model/removeContact.js
+++ model/removeContact.js
@@ -1,18 +1,16 @@
-const fs = require('fs/promises')
-const contactsPath = require('./contactsPath')
 const listContacts = require('./listContacts')
+const update = require('./update')
 
 async function removeContact(contactId) {
   try {
     const contacts = await listContacts()
-    const idx = contacts.findIndex(({ id }) => id === contactId)
+    const idx = contacts.findIndex(({ id }) => id == contactId)
     if (idx === -1) {
-      throw new Error(`Contact with id=${contactId} not found`)
+      return null
     }
-    const newContacts = contacts.filter(({ id }) => id !== contactId)
-    const updateContacts = await JSON.stringify(newContacts)
-    await fs.writeFile(contactsPath, updateContacts)
-    console.table(newContacts)
+    const newContacts = contacts.filter(({ id }) => id != contactId)
+    await update(newContacts)
+    return newContacts
   } catch (error) {
     console.error(error)
     throw error
