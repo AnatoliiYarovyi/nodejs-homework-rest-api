87a8a330e27883ce112d2331624476450024e626
Anatolii Yarovyi <apyarovyi@gmail.com>
2021-09-09 18:11:42 +0300

    Добавляет правки

--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -1,12 +1,15 @@
 const { Contact } = require('../../models')
 
 const addContact = async (req, res, next) => {
-  try {
-    const result = await Contact.create(req.body)
-    res.status(201).json({ result })
-  } catch (error) {
-    next(error)
-  }
+  const newContact = { ...req.body, owner: req.user._id }
+  const result = await Contact.create(newContact)
+  res.status(201).json({
+    status: 'success',
+    code: 201,
+    data: {
+      result,
+    },
+  })
 }
 
 module.exports = addContact
