c792472f0dafdf7a916ac55f1bc1eac9a26dbe78
Anatolii Yarovyi <apyarovyi@gmail.com>
2021-09-14 17:47:10 +0300

    Update index.js

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -3,6 +3,7 @@ const login = require('./login')
 const logout = require('./logout')
 const current = require('./current')
 const favorite = require('./favorite')
+const updateAvatars = require('./updateAvatars')
 
 module.exports = {
   signup,
@@ -10,4 +11,5 @@ module.exports = {
   logout,
   current,
   favorite,
+  updateAvatars,
 }
