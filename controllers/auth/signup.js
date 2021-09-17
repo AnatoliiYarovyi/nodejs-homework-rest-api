c7a85ff024ed7e9cc0c0e2ff978cdac0bd1645cf
Anatolii Yarovyi <apyarovyi@gmail.com>
2021-09-17 21:34:31 +0300

    Добавляет отправку email пользователю

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -3,7 +3,9 @@ const { Conflict } = require('http-errors')
 const gravatar = require('gravatar')
 const fs = require('fs/promises')
 const path = require('path')
+const { v4 } = require('uuid')
 
+const { sendMail } = require('../../utils')
 const { User } = require('../../models')
 
 const avatarsDir = path.join(__dirname, '../../', 'public/avatars')
@@ -14,17 +16,31 @@ const singup = async (req, res, next) => {
   if (user) {
     throw new Conflict('Email in use')
   }
+
+  const verifyToken = v4()
+  // --- отправка email для подтверждения регистрации -----
+  const data = {
+    to: email,
+    subject: 'Регистрация на сайте',
+    html: `<a href='http://localhost:3000/api/auth/users/verify/${verifyToken}'>Подтвердите регистрацию</a>`,
+  }
+  await sendMail(data)
+  // ------------------------------------------------------
+
   const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
   const defaultAvatar = await gravatar.url(email, { s: '250' }, true)
 
   const result = await User.create({
     email,
     password: hashPassword,
+    verifyToken,
     avatarURL: defaultAvatar,
   })
+
+  // ---- создание папки с именем id пользователя----
   const dirPath = path.join(avatarsDir, `${result._id}`)
   await fs.mkdir(dirPath)
-
+  // -------------------------------------------------
   res.status(201).json({
     status: 'success',
     code: 201,
